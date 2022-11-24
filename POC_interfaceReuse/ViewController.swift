ea9fe1a55c474b75a6e7828cd11b09de238d66da
Robson Lima Lopes <robson060814@hotmail.com>
2022-11-24 08:02:03 -0300

    Initial Commit

---
+++ POC_interfaceReuse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POC_interfaceReuse
+//
+//  Created by Robson Lima Lopes on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
